a221482f21ec4f2c7a51558d366b0752f7acf20b
Heng Li <lh3@me.com>
2015-11-11 19:55:01 -0500

    convert mhap output to PAF

---
+++ misc/mhap2paf.js
@@ -0,0 +1,19 @@
+var file = arguments.length? new File(arguments[0]) : new File();
+var buf = new Bytes();
+
+while (file.readline(buf) >= 0) {
+	var x, t = buf.toString().split(/\s+/);
+	for (var i = 5; i <= 7; ++i) t[i] = parseInt(t[i]);
+	for (var i = 9; i <= 11; ++i) t[i] = parseInt(t[i]);
+	var bl = t[6] - t[5] > t[10] - t[9]? t[6] - t[5] : t[10] - t[9];
+	var ml = Math.floor(parseFloat(t[2]) * bl / 100 + .499);
+	var cm = "cm:i:" + Math.floor(parseFloat(t[3]) + .499);
+//	if (t[4] == '1') x = t[7] - t[5], t[5] = t[7] - t[6], t[6] = x;
+//	if (t[8] == '1') x = t[11] - t[9], t[9] = t[11] - t[10], t[10] = x;
+	var rev = t[4] == t[8]? '+' : '-';
+	print([t[0], t[7], t[5], t[6], rev, t[1], t[11], t[9], t[10], ml, bl, 255, cm].join("\t"));
+	print([t[1], t[11], t[9], t[10], rev, t[0], t[7], t[5], t[6], ml, bl, 255, cm].join("\t"));
+}
+
+buf.destroy();
+file.close();
