8cefd9597667d5fab451b40cc3167d57508606de
Heng Li <lh3@me.com>
2015-11-14 18:29:23 -0500

    fix when identity is fraction not percentage

--- misc/mhap2paf.js
+++ misc/mhap2paf.js
@@ -6,7 +6,8 @@ while (file.readline(buf) >= 0) {
 	for (var i = 5; i <= 7; ++i) t[i] = parseInt(t[i]);
 	for (var i = 9; i <= 11; ++i) t[i] = parseInt(t[i]);
 	var bl = t[6] - t[5] > t[10] - t[9]? t[6] - t[5] : t[10] - t[9];
-	var ml = Math.floor(parseFloat(t[2]) * bl / 100 + .499);
+	var r = parseFloat(t[2]);
+	var ml = Math.floor((r <= 1.? bl * r : bl * r / 100) + .499);
 	var cm = "cm:i:" + Math.floor(parseFloat(t[3]) + .499);
 //	if (t[4] == '1') x = t[7] - t[5], t[5] = t[7] - t[6], t[6] = x;
 //	if (t[8] == '1') x = t[11] - t[9], t[9] = t[11] - t[10], t[10] = x;
