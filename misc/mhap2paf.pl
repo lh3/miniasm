d09b55e344583f1636516f7ea4883eb91ce3585b
Heng Li <lh3@me.com>
2015-11-14 21:10:25 -0500

    replaced js with perl

---
+++ misc/mhap2paf.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Getopt::Std;
+
+my %opts = ();
+getopts("b", \%opts);
+my $is_double = !defined($opts{b});
+
+die("Usage: mhap2paf.pl [-b] <in.mhap>\n") if (@ARGV == 0 && -t STDIN);
+
+while (<>) {
+	chomp;
+	my @t = split;
+	my $bl = $t[6] - $t[5] > $t[10] - $t[9]? $t[6] - $t[5] : $t[10] - $t[9];
+	my $r = $t[2];
+	my $ml = int($bl * ($r <= 1.? $r : .01 * $r) + .499);
+	my $cm = "cm:i:" . int($t[3] + .499);
+	my $rev = $t[4] == $t[8]? '+' : '-';
+	print(join("\t", @t[0,7,5,6], $rev, @t[1,11,9,10], $ml, $bl, 255, $cm), "\n");
+	print(join("\t", @t[1,11,9,10], $rev, @t[0,7,5,6], $ml, $bl, 255, $cm), "\n") if ($is_double);
+}
