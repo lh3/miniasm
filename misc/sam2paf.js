57742ad58e9e7434bd0705966357556973254e0d
Heng Li <lh3@me.com>
2017-07-02 16:56:24 -0400

    work with SAM generated by blasr

--- misc/sam2paf.js
+++ misc/sam2paf.js
@@ -1,6 +1,6 @@
 var file = arguments.length? new File(arguments[0]) : new File();
 var buf = new Bytes();
-var re = /(\d+)([MIDSHN])/g;
+var re = /(\d+)([MIDSHNX=])/g;
 
 var len = {}, lineno = 0;
 while (file.readline(buf) >= 0) {
@@ -21,17 +21,19 @@ while (file.readline(buf) >= 0) {
 	if (tlen == null) throw Error("ERROR at line " + lineno + ": can't find the length of contig " + t[2]);
 	var nn = (m = /\tnn:i:(\d+)/.exec(line)) != null? parseInt(m[1]) : 0;
 	var NM = (m = /\tNM:i:(\d+)/.exec(line)) != null? parseInt(m[1]) : null;
-	if (NM == null) throw Error("ERROR at line " + lineno + ": no NM tag");
+	var have_NM = NM == null? false : true;
 	NM += nn;
-	var clip = [0, 0], I = [0, 0], D = [0, 0], M = 0, N = 0, ql = 0, tl = 0;
+	var clip = [0, 0], I = [0, 0], D = [0, 0], M = 0, N = 0, ql = 0, tl = 0, mm = 0, ext_cigar = false;
 	while ((m = re.exec(t[5])) != null) {
 		var l = parseInt(m[1]);
-		if (m[2] == 'M') M += l, ql += l, tl += l;
+		if (m[2] == 'M') M += l, ql += l, tl += l, ext_cigar = false;
 		else if (m[2] == 'I') ++I[0], I[1] += l, ql += l;
 		else if (m[2] == 'D') ++D[0], D[1] += l, tl += l;
 		else if (m[2] == 'N') N += l, tl += l;
 		else if (m[2] == 'S') clip[M == 0? 0 : 1] = l, ql += l;
 		else if (m[2] == 'H') clip[M == 0? 0 : 1] = l;
+		else if (m[2] == '=') M += l, ql += l, tl += l, ext_cigar = true;
+		else if (m[2] == 'X') M += l, ql += l, tl += l, mm += l, ext_cigar = true;
 		++n_cigar;
 	}
 	if (n_cigar > 65535)
@@ -44,9 +46,10 @@ while (file.readline(buf) >= 0) {
 		warn("WARNING at line " + lineno + ": SEQ length inconsistent with CIGAR (" + t[9].length + " != " + ql + "); skipped");
 		continue;
 	}
-	if (NM < I[1] + D[1]) {
-		warn("WARNING at line " + lineno + ": NM is less than the total number of gaps (" + NM + " < " + (I[1]+D[1]) + "); skipped");
-		continue;
+	if (!have_NM || ext_cigar) NM = I[1] + D[1] + mm;
+	if (NM < I[1] + D[1] + mm) {
+		warn("WARNING at line " + lineno + ": NM is less than the total number of gaps (" + NM + " < " + (I[1]+D[1]+mm) + ")");
+		NM = I[1] + D[1] + mm;
 	}
 	var extra = ["mm:i:"+(NM-I[1]-D[1]), "io:i:"+I[0], "in:i:"+I[1], "do:i:"+D[0], "dn:i:"+D[1]];
 	var match = M - (NM - I[1] - D[1]);
